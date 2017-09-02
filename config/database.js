2d3f6bca2e78359e343278a423fceb18c8a5daef
Tung Nguyen <ngoctung@floatingcube.com>
2017-09-02 11:03:40 +0700

    fixed

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: `mongodb://ngoctung:admin@ds163053.mlab.com:63053/heroku_pl0bprl9`
+    database: `mongodb://localhost:27017/demo-mean` || `mongodb://ngoctung:admin@ds163053.mlab.com:63053/heroku_pl0bprl9`
 }
